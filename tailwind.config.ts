ebf849b5d5386045c8f1c421aa19057634fa05d5
hosenur <hosenur.dev@gmail.com>
2024-06-22 19:24:16 +0530

    ui fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,10 +6,16 @@ module.exports = {
         './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
         "./node_modules/@tailus/themer-**/dist/**/*.{js,ts}"
     ],
+    theme :{
+
+        extend: {
+            fontFamily: { "manrope": ["'Manrope Variable', sans-serif"] },
+        },
+    },
     plugins: [
         themer({
             palette: {
-                extend: "oz",
+                extend: "nature",
             },
             radius: "smoothest",
             background: "light",
